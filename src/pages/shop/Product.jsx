ffbccf4907f5e49abf07c19c2e2800fdabae51dd
Kanykei <kanisatybaldyeva@gmail.com>
2024-01-10 23:37:38 +0600

    add style for Product component, add new images link

--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
 
-export const Product = ({data}) => {
+export const Product = ({ data }) => {
   return (
-    <div>Product</div>
-  )
-}
+    <Container>
+      <img src={data.productImage} alt={data.productName} />
+      <Card>
+        <p>
+          <b>{data.productName}</b>
+        </p>
+        <p>${data.price}</p>
+      </Card>
+    </Container>
+  );
+};
+
+const Container = styled("div")`
+  width: 30%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  & > img {
+    width: 300px;
+    height: 200px;
+    border-radius: 50px;
+    align-self: center;
+  }
+`;
+
+const Card = styled("div")`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { PRODUCTS } from "../../products";
 import { Product } from "./Product";
+import { styled } from "styled-components";
 export const Shop = () => {
   return (
     <div>
       <div>
         <h1>PedroTech Shop</h1>
       </div>
-      <div>
+      <ProductItem>
         {PRODUCTS.map((product) => (
           <Product data={product} />
         ))}
-      </div>
+      </ProductItem>
     </div>
   );
 };
+
+const ProductItem = styled("div")`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
